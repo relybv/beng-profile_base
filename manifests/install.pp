efff245633ad14df30372af4c6fd83de3addb154
P. Gomersbach <p.gomersbach@rely.nl>
2016-12-02 13:40:01 +0000

    add firewall to base

--- manifests/install.pp
+++ manifests/install.pp
@@ -17,4 +17,16 @@ class profile_base::install {
     manage_repos => false,
     require => Group['puppet'],
   }
+
+  # Enable firewall
+  class { 'beng_fw' :
+    tcp_public_ports => $tcp_public_ports,
+    tcp_ports_global => $tcp_ports_global,
+    tcp_extra_rule1  => $tcp_extra_rule1,
+    tcp_rangea_src1  => $tcp_9300_source1,
+    tcp_rangea_src2  => $tcp_9300_source2,
+    tcp_rangea_src3  => $tcp_9300_source3,
+    tcp_rangeb       => $tcp_rangeb,
+    tcp_rangec       => $tcp_rangec,
+  }
 }
