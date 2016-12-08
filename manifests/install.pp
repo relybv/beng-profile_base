60d46824fa1163d724a492ec7e8cd7aee203e731
P. Gomersbach <p.gomersbach@rely.nl>
2016-12-08 15:53:22 +0000

    call fw class usig all parameters

--- manifests/install.pp
+++ manifests/install.pp
@@ -24,14 +24,21 @@ class profile_base::install {
 
   # Enable firewall
   class { 'beng_fw' :
-    tcp_public_ports => $profile_base::tcp_public_ports,
-    tcp_ports_global => $profile_base::tcp_ports_global,
-    tcp_extra_rule1  => $profile_base::tcp_extra_rule1,
-    tcp_rangea_src1  => $profile_base::tcp_rangea_src1,
-    tcp_rangea_src2  => $profile_base::tcp_rangea_src2,
-    tcp_rangea_src3  => $profile_base::tcp_rangea_src3,
-    tcp_rangeb       => $profile_base::tcp_rangeb,
-    tcp_rangec       => $profile_base::tcp_rangec,
+    tcp_public_ports       => $profile_base::tcp_public_ports,
+    tcp_ports_global       => $profile_base::tcp_ports_global,
+    tcp_rangea_src1        => $profile_base::tcp_rangea_src1,
+    tcp_rangea_src2        => $profile_base::tcp_rangea_src2,
+    tcp_rangea_src3        => $profile_base::tcp_rangea_src3,
+    tcp_rangeb             => $profile_base::tcp_rangeb,
+    tcp_rangec             => $profile_base::tcp_rangec,
+    internal_neta          => $profile_base::internal_neta,
+    internal_netb          => $profile_base::internal_netb,
+    internal_netc          => $profile_base::internal_netc,
+    tcp_ports              => $profile_base::tcp_ports_global,
+    udp_ports              => $profile_base::udp_ports,
+    tcp_extra_rule1        => $profile_base::tcp_extra_rule1,
+    tcp_extra_rule1_dport  => $profile_base::tcp_extra_rule1_dport,
+    tcp_extra_rule1_source => $profile_base::tcp_extra_rule1_source,
   }
 
   # Create group and enable sudo
