4ce4efbe8d5787763f47ede8fcdcfa3a8dac8fb5
P. Gomersbach <p.gomersbach@rely.nl>
2016-12-08 13:59:19 +0000

    pass parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,15 +9,20 @@
 #
 class profile_base
 (
-  $ntpservers       = $::profile_base::params::ntpservers,
+  $ntpservers = $::profile_base::params::ntpservers,
   $tcp_public_ports = $::profile_base::params::tcp_public_ports,
   $tcp_ports_global = $::profile_base::params::tcp_ports_global,
-  $tcp_extra_rule1  = $::profile_base::params::tcp_extra_rule1,
-  $tcp_rangea_src1  = $::profile_base::params::tcp_rangea_src1,
-  $tcp_rangea_src2  = $::profile_base::params::tcp_rangea_src2,
-  $tcp_rangea_src3  = $::profile_base::params::tcp_rangea_src3,
-  $tcp_rangeb       = $::profile_base::params::tcp_rangeb,
-  $tcp_rangec       = $::profile_base::params::tcp_rangec,
+  $tcp_extra_rule1 = $::profile_base::params::tcp_extra_rule1,
+  $tcp_rangea_src1 = $::profile_base::params::tcp_rangea_src1,
+  $tcp_rangea_src2 = $::profile_base::params::tcp_rangea_src2,
+  $tcp_rangea_src3 = $::profile_base::params::tcp_rangea_src3,
+  $tcp_rangeb = $::profile_base::params::tcp_rangeb,
+  $tcp_rangec = $::profile_base::params::tcp_rangec,
+  $internal_neta = $::beng_fw::params::internal_neta,
+  $internal_netb = $::beng_fw::params::internal_netb,
+  $internal_netc = $::beng_fw::params::internal_netc,
+  $tcp_ports = $::beng_fw::params::tcp_ports_global,
+  $udp_ports = $::beng_fw::params::udp_ports,
 ) inherits ::profile_base::params {
 
 
--- manifests/params.pp
+++ manifests/params.pp
@@ -6,11 +6,19 @@
 class profile_base::params {
   $ntpservers = [ '172.18.99.210', '172.18.99.211' ]
   $tcp_public_ports = false
-  $tcp_ports_global = false
+  $tcp_ports_global = [ '20','21','22','80','443','445','1556','5666','8000','9100','9200','13720','13724' ]
   $tcp_extra_rule1 = false
   $tcp_rangea_src1 = false
   $tcp_rangea_src2 = false
   $tcp_rangea_src3 = false
-  $tcp_rangeb = false
-  $tcp_rangec = false
+  $tcp_9300_source1 = false
+  $tcp_9300_source2 = false
+  $tcp_9300_source3 = false
+  $tcp_rangeb = '8080-8095'
+  $tcp_rangec = '8011-8015'
+  $internal_neta = '172.18.0.0/16'
+  $internal_netb = '172.19.0.0/16'
+  $internal_netc = '178.249.248.128/25'
+  $udp_ports = [ '53','123','161']
+  $tcp_rangea_ports = false
 }
